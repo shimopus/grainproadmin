458a9c9b854bb852096d466cebc30a3e46d927f3
Sergey <shimopus@gmail.com>
2018-02-08 22:11:07 +0300

    Change urls from email to open site with station already selected

--- src/main/java/pro/grain/admin/web/pages/MarketTableController.java
+++ src/main/java/pro/grain/admin/web/pages/MarketTableController.java
@@ -1,6 +1,7 @@
 package pro.grain.admin.web.pages;
 
 import com.codahale.metrics.annotation.Timed;
+import org.apache.http.client.utils.URIBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
@@ -73,12 +74,18 @@ public class MarketTableController {
         String baseUrl = grainProAdminProperties.getSite().getBaseUrl()
             + (bidType == BidType.SELL ?
             grainProAdminProperties.getSite().getSellPagePath() :
-            grainProAdminProperties.getSite().getBuyPagePath())
-            + (code != null && !"".equals(code) ? "?code=" + code : "");
+            grainProAdminProperties.getSite().getBuyPagePath());
+
+        URIBuilder uri = new URIBuilder(baseUrl)
+            .setParameter("from", "file");
+
+        if (code != null && !"".equals(code)) {
+            uri.setParameter("code", code);
+        }
 
         return ResponseEntity.ok()
             .contentType(MediaType.APPLICATION_OCTET_STREAM)
-            .body(marketService.getMarketTableHTML(code, bidType,"market_table_download", baseUrl));
+            .body(marketService.getMarketTableHTML(code, bidType,"market_table_download", uri.toString()));
     }
 
     @RequestMapping(value = "/market-table/email-inside",
@@ -88,18 +95,25 @@ public class MarketTableController {
     public ResponseEntity<String> emailInsideAllRegions(
         @RequestParam(value = "rowsLimit", required = false) Integer rowsLimit,
         @RequestParam(value = "code", required = false) String code,
-        @RequestParam("bidType") BidType bidType) {
+        @RequestParam("bidType") BidType bidType) throws URISyntaxException {
         log.debug("REST request to email inside list of bids for station code {}", code);
 
         String baseUrl = grainProAdminProperties.getSite().getBaseUrl()
             + (bidType == BidType.SELL ?
-                grainProAdminProperties.getSite().getSellPagePath() :
-                grainProAdminProperties.getSite().getBuyPagePath())
-            + (code != null && !"".equals(code) ? "?code=" + code : "");
+            grainProAdminProperties.getSite().getSellPagePath() :
+            grainProAdminProperties.getSite().getBuyPagePath());
+
+        URIBuilder uri = new URIBuilder(baseUrl)
+            .setParameter("from", "email");
+
+        if (code != null && !"".equals(code)) {
+            uri.setParameter("code", code);
+        }
 
         int rowsLimitI = rowsLimit == null ? -1 : rowsLimit;
         return ResponseEntity.ok()
             .contentType(MediaType.APPLICATION_OCTET_STREAM)
-            .body(marketService.getMarketTableHTML(code, bidType,"market_table_email_inside", baseUrl, rowsLimitI));
+            .body(marketService.getMarketTableHTML(code, bidType,"market_table_email_inside",
+                uri.toString(), rowsLimitI));
     }
 }
